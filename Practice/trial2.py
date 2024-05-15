bdf5705fe87e9876ea7636a151d1bf146a0bf56b
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-05-15 22:00:51 +0300

    exercise2 on booleans

---
+++ Practice/trial2.py
@@ -0,0 +1,10 @@
+# write a program that asks for the age and stores it in a variable 
+# Then, use a boolean to check for if they are above 18(True) or not False
+
+
+age = int(input("How old are you? "))
+
+if age > 18:
+    print(True) 
+else:
+    print(False)
