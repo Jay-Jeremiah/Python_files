5f9c07747ae05d4ef25ad9420467359bab3e0b58
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-05-17 17:29:46 +0300

    Input practice in python

---
+++ Practice/trial3.py
@@ -0,0 +1,9 @@
+# Write a program to greet the user by name andask for their favorite color.
+
+name = input("Enter your name here please: ")
+
+print("Hello", name)
+
+color = input("What is your favorite color?: ")
+
+print("Favorite color: ", color)
