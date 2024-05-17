2a17ef26ffadcd0428ed1c3697f31e0d9ec7fd34
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-05-17 08:16:18 +0300

    list properties in python

---
+++ Lists/li.py
@@ -0,0 +1,10 @@
+# lists are used to store multiple items in a single variable
+# created using square brackets
+
+fruits = ["Bananas","Apples","Grapes"]
+
+print("Here are some examples of fruits: ", fruits)
+
+# we can also find the length of our list
+
+print("The length of the fruits is: ", len(fruits))
