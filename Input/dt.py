9ede0bf322322bc3b9aaaae035f9d81daa4a816e
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-05-24 15:30:00 +0300

    Input practice with date of birth

---
+++ Input/dt.py
@@ -0,0 +1,8 @@
+Current_year = int(input("Enter the current year: "))
+
+Birth_year = int(input("Enter your year of birth: "))
+
+Difference = Current_year - Birth_year
+
+print(f"You are {Difference} years old now.")
+
