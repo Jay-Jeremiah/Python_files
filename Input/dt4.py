e6fd2ee47d860ea799a72de3212d91f00938856d
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-05-24 16:28:35 +0300

    Perimeter and Area of a rectangle

---
+++ Input/dt4.py
@@ -0,0 +1,11 @@
+L = int(input('Enter length : '))
+
+B = int(input("Enter breadth: "))
+
+
+Area = L * B
+
+Perimeter = 2*(L+B)
+
+print(f"Area of rectangle {Area}")
+print(f"Perimeter of rectangle {Perimeter}")
