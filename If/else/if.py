1e263b25b0964f5dbb497b90cb498a92e4338298
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-05-31 09:52:45 +0300

    if statement

---
+++ If/else/if.py
@@ -0,0 +1,9 @@
+#if staements are conditional
+#if the conditional is met, the statement is executed.
+
+number = 10
+
+if number >= 0:
+    print("This is a positive negative")
+
+print('It is a negative number.')
