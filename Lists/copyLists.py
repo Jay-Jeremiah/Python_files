db0a1a58ff0c786630f0d3f2dcffb85c05ecfd10
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-06-11 12:12:37 +0300

    copying lists

---
+++ Lists/copyLists.py
@@ -0,0 +1,15 @@
+#copy() is used to copy lists
+
+fruits = ['apple','pineapple','cherry']
+
+other = fruits.copy()
+
+print(other)
+
+#we can use also use list to copy lists
+
+colours = ['red','green','brown']
+
+colours1 = list(colours)
+
+print(colours1)
