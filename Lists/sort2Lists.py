00c51732138fea34360bd807ebbf5eb6a2f9ecd0
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-06-11 11:17:54 +0300

    sorting lists in python

---
+++ Lists/sort2Lists.py
@@ -0,0 +1,22 @@
+#sort(reserve=True) - sorts the listin descending order
+
+men_wear = ['Shirt','pants','Watch','undergarments']
+
+men_wear.sort(reverse=True)
+
+print(men_wear)
+
+print('\n')
+
+numbers = [12,54,78,23,100,8,34]
+
+numbers.sort(reverse=True)
+
+print(numbers)
+
+print('\n')
+#reverse() - arranges the list from the last item
+
+men_wear.reverse()
+
+print(men_wear)
