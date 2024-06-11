30d7029c232309e78fbda334c6f0a4e3975ec553
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-06-11 17:07:23 +0300

    tuples in python

---
+++ Tuples/Tuple.py
@@ -0,0 +1,13 @@
+#identified by () and immutable
+
+clothes = ('pants','shorts','cargo-pants')
+
+print(clothes)
+
+print(type(clothes))
+
+hi = len(clothes)
+
+print("The length of the tuple is",hi)
+
+
