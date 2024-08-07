c0f0c6fc1ac32cb405b12f65f63c19a30da08b8a
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-08-07 18:59:17 +0300

    init function in classes

--- Classes/init.py
+++ Classes/init.py
@@ -1,3 +1,13 @@
 #__init__ function - initiates the classes 
 
-class
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
+
